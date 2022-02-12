ebe6958f6c119ffe1934f5e7b226865492f30e52
jdjmr15 <jmarquezcl1@gmail.com>
2022-02-12 20:43:50 -0300

    [UPD] Culmina el ejercicio

--- src/main.go
+++ src/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main () {
+import "fmt"
 
+func main() {
+	//defer (Cierra conexcion y archivo)
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
+
+	i := 0
+	for i < 10 {
+
+		if i == 2 {
+			i++
+			continue
+		}
+
+		if i == 8 {
+			break
+		}
+		fmt.Println(i)
+		i++
+	}
 }
