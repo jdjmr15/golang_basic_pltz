2396b9e09efc867395beeaf9c68ea5002bf76183
jdjmr15 <jmarquezcl1@gmail.com>
2022-02-12 21:41:26 -0300

    [END] ejericio de palidromo, libreria strings y iteraccion de cadenas

--- src/main.go
+++ src/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main () {
+import (
+	"fmt"
+	"strings"
+)
+
+func validarPalidromo(texto string) string {
+	var textoAlreves string
+	fmt.Printf("Texto %s: %d\n", texto, len(texto))
+	for i := (len(texto)) - 1; i >= 0; i-- {
+		textoAlreves += string(texto[i])
+	}
+
+	if strings.ToUpper(textoAlreves) == strings.ToUpper(texto) {
+		return "Palidromo"
+	} else {
+		return "No Palidromo"
+	}
+}
+
+func main() {
+
+	fmt.Println(validarPalidromo("Amor a Roma"))
 
 }
