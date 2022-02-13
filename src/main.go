f48d969a1135c10fcc53559cd01d36f2cd8cf5a4
jdjmr15 <jmarquezcl1@gmail.com>
2022-02-12 22:02:16 -0300

    [END] map es igual diccionario en python

--- src/main.go
+++ src/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main () {
+import "fmt"
 
+func main() {
+	diccionario := make(map[string]int)
+	diccionario["Jose"] = 14
+	diccionario["Pepito"] = 20
+	
+	for k,v := range diccionario {
+		fmt.Println(k,v)
+	}	
+
+	// Valor existe
+	value, ok := diccionario["Jose"]
+	fmt.Println(value,ok)
+	value, ok = diccionario["Jesus"]
+	fmt.Println(value,ok)
 }
