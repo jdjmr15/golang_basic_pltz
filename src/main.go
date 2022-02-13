5cd91adc54f47aee4bed12134f984c7a924d1b4f
jdjmr15 <jmarquezcl1@gmail.com>
2022-02-12 23:22:36 -0300

    [END] Struct su modificadores y paquete

--- src/main.go
+++ src/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main () {
+// Se deactivo <go env -w GO111MODULE=off>
+import (
+	"fmt"
+	auto "mipaquete"
+)
 
+/*
+Se move a la carpeta mipaquete
+type auto struct {
+	marca  string
+	anio   int
+	color  string
+	modelo string
+}
+
+func main() {
+
+	fiesta := auto{marca: "Ford", color: "Azul", anio: 2021, modelo: "turbo"}
+	fmt.Println(fiesta)
+
+	var corsa auto
+	corsa.marca = "Cherolet"
+	fmt.Println(corsa)
+
+}
+*/
+
+func main() {
+	var fiesta auto.AutoPublico
+	fiesta.Marca = "Ford"
+	fmt.Println(fiesta)
 }
---
+++ src/mipaquete/mipaquete.go
@@ -0,0 +1,13 @@
+package mipaquete
+
+// Auto relacionado la descrip de los autos. Estructura publica
+type AutoPublico struct {
+	Marca string
+	Anio  int
+}
+
+// Estructura privada
+type autoPrivado struct {
+	marca string
+	anio  int
+}
